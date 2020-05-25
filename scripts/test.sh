57fa52e1627f7deaea8a906e767863d04b9f8a4a
Ryan Morshead <ryan.morshead@gmail.com>
2020-05-24 17:05:43 -0700

    explicitely close file (#19)

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,6 @@ set -e
 
 pytest --cov=pyalect --cov-config=.coveragerc
 black --verbose --check .
-flake8 src/py
+flake8 pyalect tests
 mypy pyalect --strict
 sphinx-build -b html docs/source docs/build
